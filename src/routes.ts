f4da0f684f6782f49db139a0b6e04594ff52b482
Iury <iuryalima1@gmail.com>
2023-11-06 23:45:26 -0300

    :sparkles: List-Properties

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ import { transporter } from "./services/nodemailer/Transport";
 import { sendEmailWelcome } from "./controllers/nodemailer/nodemailerController";
 import { PurchaseTransactionController } from "./controllers/Transactions/PurchaseTransactionController";
 import { RentalTransactionController } from "./controllers/Transactions/RentalTransactionController";
+import { ListPropertiesService } from "./services/properties/ListPropertiesService";
 
 
 
@@ -26,6 +27,19 @@ router.get('/userinfo', isAuthAtheticated, new DetailUserController().handle)
 
 // ---- ROTAS PROPERTIES ----
 router.post('/addproperty', upload.array('file', 4), new CreatePropertiesController().handle)
+router.get('/userproperties/:userId', isAuthAtheticated, async (req, res) => {
+  const userId = req.params.userId; // Supondo que o userId é passado como parte da URL
+
+  try {
+      const listPropertiesService = new ListPropertiesService();
+      const userProperties = await listPropertiesService.execute(userId);
+
+      res.json(userProperties);
+  } catch (error) {
+      console.error('Erro ao listar as propriedades do usuário:', error);
+      res.status(500).json({ error: 'Erro interno de servidor ao listar propriedades' });
+  }
+});
 
 // ---- ROTAS DE EMAILS ----
 router.post('/send-welcome-email', async (req, res) => {
